0acecc43520ce55464deafbba4b3782d028bf7e5
Matvey <empflow@tutanota.com>
2023-10-06 17:36:38 +0300

    refactor(server): remove unnecessary `async`

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,6 @@ import "express-async-errors";
 import app from "./app";
 const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
