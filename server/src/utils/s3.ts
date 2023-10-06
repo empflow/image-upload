34163ef1ddf4943e92038103e2bcf01d5300b40b
Matvey <empflow@tutanota.com>
2023-10-06 17:38:53 +0300

    feat(client): improve s3Upload()
    make s3Upload() figure out the content type and also find the correct file extension from the filename passed to it

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -1,6 +1,9 @@
 import S3 from "aws-sdk/clients/s3";
 import getEnvVar from "./getEnvVar";
 import getS3FileNames from "./getS3FileNames";
+import mime from "mime-types";
+import getContentType from "./getContentType";
+import getFileExt from "./getFileExt";
 
 const accessKeyId = getEnvVar("S3_ACCESS_KEY_ID");
 const secretAccessKey = getEnvVar("S3_SECRET_ACCESS_KEY");
@@ -17,17 +20,31 @@ const s3 = new S3({
   s3ForcePathStyle: true,
 });
 
+/**
+ *
+ * @param body the file itself
+ * @param filename this is needed to get the file extension like .jpeg or .png or .md or whatever
+ */
 export async function s3Upload(
   body: Buffer | Uint8Array | Blob | string,
-  ext: string
+  filename: string
 ) {
   try {
+    const ext = getFileExt(filename);
     const key = getS3FileNames(ext);
+    const contentType = getContentType(ext);
+
     const result = await s3
-      .upload({ Bucket: bucketName, Key: key, Body: body })
+      .upload({
+        Bucket: bucketName,
+        Key: key,
+        Body: body,
+        ContentType: contentType,
+      })
       .promise();
+
     return result;
   } catch (err) {
-    throw new Error("upload error");
+    throw new Error("Upload error");
   }
 }
